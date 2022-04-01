58819306daa65c4a70d5a417283d2c582e811044
Berat <64924845+beratbaal@users.noreply.github.com>
2022-04-01 22:22:08 +0300

    Adding sign up screen

--- src/Views/SignUp.jsx
+++ src/Views/SignUp.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-
+import "../css/signup.css";
 
 export class SignUp extends React.Component {
     constructor(props) {
         super(props);
     }
-    render(){
-        return(
-            <div>
-                dqwdqwdqw
+    render() {
+        return (
+            <div className="signupbody">
+
+                <img src="https://assets.dsmartgo.com.tr/content/img/dsmart-logo.svg" className="logosignup" />
+                <div className="signupform">
+                    <h3 className="signupheader">Üye Ol</h3>
+                    <p className="signuptext">D-Smart Abonesi misiniz?</p>
+                    <div className="signupbutton">
+                        <button className="btnevet">Evet</button>
+                        <button className="btnhayır">Hayır</button>
+                    </div>
+                    <p className="signuptext2">Hemen D-Smart GO’ya ücretsiz üye olabilir, 1 ay boyunca Film Dizi paketinizin keyfini çıkarabilirsiniz.</p>
+                    
+                        <span className="signuplabel">Telefon Numarası</span>
+                        <input type="text"  className="signupinput" placeholder="5XX XXX XX XX"/>
+                    
+                </div>
+
+
+
             </div>
         );
     }
