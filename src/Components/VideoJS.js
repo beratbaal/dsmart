5a077011502686ae48420847d7a6eb3aec7b4c84
Berat <64924845+beratbaal@users.noreply.github.com>
2022-04-10 18:54:18 +0300

    videojs updated

--- src/Components/VideoJS.js
+++ src/Components/VideoJS.js
@@ -3,50 +3,25 @@ import '../css/video.css';
 import VideoPlayer from 'react-video-js-player';
 
 
-const skipTime = 15;
+
 
 function VideoJS({
     src,
     poster,
     autoplay,
-    onPlay,
-    onPause,
-    onEnd
+   
 }) {
 
-    const handle=(player)=>{
-
-        player.on('play', () => {
-            onPlay();
-        });
-        player.on('pause', () => {
-            onPause();
-        });
-        player.on('ended', () => {
-            onEnd();
-        });
-        document.addEventListener("keydown",(e)=>{
-            if(e.keyCode==37){
-                skip(player)
-            }else if(e.keyCode==39){
-                skip(player)
-            }
-          }
-        )
-    }
-    function skip(player){
-        player.currentTime( Math.floor(player.currentTime()+skipTime));
-    }
+  
 
   return (
     <div className='video-js'>
         <VideoPlayer
             src={src}
             poster={poster}
-            playbackRates={[0.25, 0.50, 1, 1.25, 1.50]}
+            playbackRates={[0.25, 0.50, 1, 1.25, 1.50, 2]}
             autoplay={autoplay}
-            controls
-            onReady={handle}
+          
         />
     </div>
   )
